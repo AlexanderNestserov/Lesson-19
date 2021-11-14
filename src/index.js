a7eb6daad21fe09df3bd6f451d861881f2c82c33
Alexander Nestserov <alexerdeader@gmail.com>
2021-11-14 09:23:42 +0300

    l21

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import timer from './modules/timer';
 import menu from './modules/menu';
 import modal from './modules/modal';
 
-timer('10 november 2021');
+timer('14 november 2021');
 menu();
 modal();
