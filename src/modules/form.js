cf09889554ac2ea2257e757b234ed538cd6c0e55
Alexander Nestserov <alexerdeader@gmail.com>
2021-11-17 23:00:45 +0300

    les21+

--- src/modules/form.js
+++ src/modules/form.js
@@ -16,65 +16,23 @@ const form = () => {
 
    form1.addEventListener('input', (e) => {
       e.preventDefault();
-      e.target.value = e.target.value.replace(/[^\s\-\а-яА-Я]+/g, "");
-      // if (!/([^\s\-\а-яА-Я])+/g.test(formName1.value) && formName1.value !== ///"") 
-      if (/[\-\_\!\.\~\*\'\w]+@([\w]+\.)+[\w]+/gi.test(formEmail1.value) && formEmail1.value !== "") {
-         alert('Email');
-      } else {
-
-      }
-      if (!/[^\(\)\-\d]+/g.test(formPhone1.value) && formPhone1.value !== "") {
-         alert('Тел');
-      } else {
-
-      }
-
+      formName1.value = formName1.value.replace(/[^\s\-\а-яА-Я]+/g, "");
+      formEmail1.value = formEmail1.value.replace(/[^\@\-\_\!\.\~\*\'\w]+/gi, "");
+      formPhone1.value = formPhone1.value.replace(/[^\(\)\-\d]+/g, "");
    });
 
-   form2.addEventListener('submit', (e) => {
+   form2.addEventListener('input', (e) => {
       e.preventDefault();
-      let isError = false;
-      if (!/([^\s\-\а-яА-Я])+/g.test(formName2.value) && formName2.value !== "") {
-         alert('Кириллица');
-      } else {
-         isError = true;
-      }
-      if (/[\-\_\!\.\~\*\'\w]+@([\w]+\.)+[\w]+/gi.test(formEmail2.value) && formEmail2.value !== "") {
-         alert('Email');
-      } else {
-         isError = true;
-      }
-      if (!/[^\(\)\-\d]+/g.test(formPhone2.value) && formPhone2.value !== "") {
-         alert('Тел');
-      } else {
-         isError = true;
-      }
-      if (!isError) {
-         alert('Данные отправлены');
-      }
+      formName2.value = formName2.value.replace(/[^\s\-\а-яА-Я]+/g, "");
+      formEmail2.value = formEmail2.value.replace(/[^\@\-\_\!\.\~\*\'\w]+/gi, "");
+      formPhone2.value = formPhone2.value.replace(/[^\(\)\-\d]+/g, "");
    });
 
-   form3.addEventListener('submit', (e) => {
+   form3.addEventListener('input', (e) => {
       e.preventDefault();
-      let isError = false;
-      if (!/([^\s\-\а-яА-Я])+/g.test(formName3.value) && formName3.value !== "") {
-         alert('Кириллица');
-      } else {
-         isError = true;
-      }
-      if (/[\-\_\!\.\~\*\'\w]+@([\w]+\.)+[\w]+/gi.test(formEmail3.value) && formEmail3.value !== "") {
-         alert('Email');
-      } else {
-         isError = true;
-      }
-      if (!/[^\(\)\-\d]+/g.test(formPhone3.value) && formPhone3.value !== "") {
-         alert('Тел');
-      } else {
-         isError = true;
-      }
-      if (!isError) {
-         alert('Данные отправлены');
-      }
+      formName3.value = formName3.value.replace(/[^\s\-\а-яА-Я]+/g, "");
+      formEmail3.value = formEmail3.value.replace(/[^\@\-\_\!\.\~\*\'\w]+/gi, "");
+      formPhone3.value = formPhone3.value.replace(/[^\(\)\-\d]+/g, "");
    });
 };
 
